c9a574b956a193bbe4e3e62772f45af2af780c11
Ryan West <west.ryan.k@gmail.com>
2023-07-21 14:54:57 -0600

    Separate Powershell Prompts with "-------"

--- setup_poshgit.ps1
+++ setup_poshgit.ps1
@@ -1,3 +1,4 @@
 PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
 Add-PoshGitToProfile -AllUsers -AllHosts 
 '$GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true' | Add-Content $PROFILE.AllUsersAllHosts
+'$GitPromptSettings.DefaultPromptPrefix.Text = "---------------------------------------`n$($GitPromptSettings.DefaultPromptPrefix.Text)"' | Add-Content $PROFILE.AllUsersAllHosts
