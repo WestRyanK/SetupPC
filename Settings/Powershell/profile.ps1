57b7467fa8771966a54ec7817505a172438d9127
Ryan West <west.ryan.k@gmail.com>
2023-07-26 12:56:28 -0600

    **Commit Powershell Settings**

--- Settings/Powershell/profile.ps1
+++ Settings/Powershell/profile.ps1
@@ -1,5 +1,4 @@
 Import-Module posh-git
-$GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
-$GitPromptSettings.DefaultPromptPrefix.Text = "---------------------------------------`n$($GitPromptSettings.DefaultPromptPrefix.Text)"
 Set-PSReadLineKeyHandler -Chord 'Ctrl+j' -Function NextHistory
 Set-PSReadLineKeyHandler -Chord 'Ctrl+k' -Function PreviousHistory
+oh-my-posh init pwsh --config "$home/.oh_my_posh.omp.json" | iex
