ee4bc0b822e6cf5e94477e5e133a0010e2a7aeba
Ryan West <Ryan.West@novarad.net>
2023-07-28 16:56:43 -0600

    **Backup Powershell: Bind Capslock/PageDown to Prediction Complete**

--- Settings/Powershell/profile.ps1
+++ Settings/Powershell/profile.ps1
@@ -2,6 +2,6 @@ Import-Module posh-git
 Import-Module SettingsRepo -DisableNameChecking
 Set-PSReadLineKeyHandler -Chord 'Ctrl+j' -Function NextHistory
 Set-PSReadLineKeyHandler -Chord 'Ctrl+k' -Function PreviousHistory
-# Use CapsLock for prediction completion when CapsLock is bound to NumpadAdd with Registry Tweak
-Set-PSReadLineKeyHandler -Chord 'Add' -Function ForwardChar 
+# Use CapsLock for prediction completion when CapsLock is bound to PageDown with Registry Tweak
+Set-PSReadLineKeyHandler -Chord 'PageDown' -Function ForwardChar 
 oh-my-posh init pwsh --config "$home/.oh_my_posh.omp.json" | iex
