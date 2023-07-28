2f9879e3523cd35dce0123a0219137e95cd33a64
Ryan West <Ryan.West@novarad.net>
2023-07-28 16:38:27 -0600

    **Backup Powershell: Bind Capslock/NumpadAdd to Prediction Completion**

--- Settings/Powershell/profile.ps1
+++ Settings/Powershell/profile.ps1
@@ -2,4 +2,6 @@ Import-Module posh-git
 Import-Module SettingsRepo -DisableNameChecking
 Set-PSReadLineKeyHandler -Chord 'Ctrl+j' -Function NextHistory
 Set-PSReadLineKeyHandler -Chord 'Ctrl+k' -Function PreviousHistory
+# Use CapsLock for prediction completion when CapsLock is bound to NumpadAdd with Registry Tweak
+Set-PSReadLineKeyHandler -Chord 'Add' -Function ForwardChar 
 oh-my-posh init pwsh --config "$home/.oh_my_posh.omp.json" | iex
