ad967c17c7656ce0db10a1ba10d272358483424f
Ryan West <Ryan.West@novarad.net>
2023-07-28 16:33:09 -0600

    **Backup Powershell: Switch from RepoSettings to SettingsRepo**

--- Settings/Powershell/profile.ps1
+++ Settings/Powershell/profile.ps1
@@ -1,5 +1,5 @@
 Import-Module posh-git
-Import-Module RepoSettings -DisableNameChecking
+Import-Module SettingsRepo -DisableNameChecking
 Set-PSReadLineKeyHandler -Chord 'Ctrl+j' -Function NextHistory
 Set-PSReadLineKeyHandler -Chord 'Ctrl+k' -Function PreviousHistory
 oh-my-posh init pwsh --config "$home/.oh_my_posh.omp.json" | iex
