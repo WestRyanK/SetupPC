6fe47c3d3f5af2639ddc59ef1a8a50dd2369239a
Ryan West <west.ryan.k@gmail.com>
2023-08-04 13:24:50 -0600

    Add NeoVim to Vim Settings

--- Settings/Vim.ps1
+++ Settings/Vim.ps1
@@ -1,10 +1,20 @@
 $BackupPath = "$PSScriptRoot/Vim"
+$NVimConfigDir = "$env:LocalAppData/nvim"
+$NVimConfigPath = "$NVimConfigDir/init.vim"
 
 Function Backup {
     Copy-Item -Path "$Home/_vimrc" -Destination $BackupPath
     Copy-Item -Path "$Home/_ideavimrc" -Destination $BackupPath
+    if (Test-Path $NVimConfigPath) {
+        Copy-Item -Path "$NVimConfigDir/init.vim" -Destination $BackupPath
+    }
 }
 
 Function Restore {
-    Copy-Item -Path "$BackupPath/*" -Destination $Home
+    Copy-Item -Path "$BackupPath/_vimrc" -Destination $Home
+    Copy-Item -Path "$BackupPath/_ideavimrc" -Destination $Home
+    if (Test-Path "$BackupPath/init.vim") {
+        $null = New-Item -ItemType Directory -Path $NVimConfigDir -Force
+        Copy-Item -Path "$BackupPath/init.vim" -Destination $NVimConfigDir
+    }
 }
