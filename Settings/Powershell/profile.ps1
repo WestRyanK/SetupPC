16a1577cb163d0bf7eaaa957c29284d581ff7159
Ryan West <west.ryan.k@gmail.com>
2023-12-12 12:41:27 -0700

    **Backup Powershell: Remove Vi mode from Powershell**

--- Settings/Powershell/profile.ps1
+++ Settings/Powershell/profile.ps1
@@ -15,51 +15,14 @@ if ($interactive) {
     Set-Alias -Name vim -Value nvim
     Set-Alias -Name vi -Value nvim
     
-    function OnViModeChange {
-        if ($args[0] -eq 'Command') {
-            # Set the cursor to a blinking block.
-            Write-Host -NoNewLine "`e[1 q"
-        } else {
-            # Set the cursor to a blinking line.
-            Write-Host -NoNewLine "`e[5 q"
-        }
-    }
-    Set-PSReadLineOption -EditMode vi
-    Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $Function:OnViModeChange
     Set-PSReadLineOption -BellStyle visual
-    if ((Get-Module PSReadLine).PrivateData.PSData.PreRelease -ne $null) {
-        Set-PSReadLineOption -ViClipboardMode SystemClipboard
-    }
-    
-# Use two quick presses of the 'j' key to exit Vim Insert mode in the PowerShell prompt
-    $LastJTime = $null
-    Set-PSReadLineKeyHandler -Chord j -ViMode Insert -ScriptBlock {
-        $LastJTime = $script:LastJTime
-        $script:LastJTime = Get-Date
-    
-        if ($LastJTime -eq $null) {
-            $LastJTime = (Get-Date -Year 1 -Month 1 -Day 1)
-        }
-    
-        $TimeSinceLastJ = New-TimeSpan -Start $LastJTime -End (Get-Date)
-    
-        if (($TimeSinceLastJ.TotalSeconds) -lt .25) {
-            [Microsoft.PowerShell.PSConsoleReadLine]::BackwardDeleteChar()
-            [Microsoft.PowerShell.PSConsoleReadLine]::ViCommandMode()
-        }
-        else {
-            [Microsoft.PowerShell.PSConsoleReadLine]::Insert('j')
-        }
-    }
     
 # Use Tab for prediction completion and Capslock/Shift+Capslock to cycle options.
 # Only works when Capslock is bound to the PageDown key with Registry Tweak
     Remove-PSReadLineKeyHandler -Chord 'Shift+Tab'
     Set-PSReadLineKeyHandler -Chord 'Tab' -Function ForwardChar
     Set-PSReadLineKeyHandler -Chord 'PageDown' -Function TabCompleteNext
-    Set-PSReadLineKeyHandler -Chord 'PageDown' -Function ViTabCompleteNext
     Set-PSReadLineKeyHandler -Chord 'Shift+PageDown' -Function TabCompletePrevious
-    Set-PSReadLineKeyHandler -Chord 'Shift+PageDown' -Function ViTabCompletePrevious
 
     oh-my-posh init pwsh --config "$home/.oh_my_posh.omp.json" | iex
 } 
