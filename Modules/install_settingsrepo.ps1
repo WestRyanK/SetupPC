8c78a12ee6fdc8b41f1870cd58fcb62d82da0191
Ryan West <west.ryan.k@gmail.com>
2026-03-23 12:48:08 -0600

    Use environment's MyDocuments path for install
    Uses the "My Documents" path returned for the current environment
    instead of assuming a hardcoded path of `$HOME/Documents`. This resolves
    an issue where the module is installed in the wrong directory because
    the user's Documents folder is configured to a different path.

--- Modules/install_settingsrepo.ps1
+++ Modules/install_settingsrepo.ps1
@@ -1,2 +1,3 @@
-Copy-Item -Path "$PSScriptRoot/SettingsRepo" -Destination "$Home/Documents/PowerShell/Modules" -Recurse -Force
+$DocumentsPath = [Environment]::GetFolderPath("MyDocuments")
+Copy-Item -Path "$PSScriptRoot/SettingsRepo" -Destination "$DocumentsPath/PowerShell/Modules" -Recurse -Force
 Import-Module SettingsRepo -DisableNameChecking -Force
